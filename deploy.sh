35ad7141c2a25a264d2b80b1b2db3000cb2c33e1
Lewis Webb <webblcw23@hotmail.com>
2025-10-22 04:31:14 +0100

    reorder and rename

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # Rezip and then deploy to web app
 
 zip -r publish.zip .
-az webapp deployment source config-zip \
-  --resource-group lewis-webb-portfolio-webpage-rg \
+az webapp deploy --resource-group lewis-webb-portfolio-webpage-rg \
   --name lewis-webb-portfolio-webpage \
-  --src publish.zip
+  --src-path publish.zip --type zip
+
