1d12632c93112f2186db290b2f4e21340b94bf11
Lewis Webb <webblcw23@hotmail.com>
2025-10-25 06:25:38 +0100

    tidy zipped folder - added 3 tier M.E

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 # Rezip and then deploy to web app
 
-zip -r publish.zip .
+zip -r publish.zip . -x "*.git*" "*.DS_Store"
 az webapp deploy --resource-group lewis-webb-portfolio-webpage-rg \
   --name lewis-webb-portfolio-webpage \
   --src-path publish.zip --type zip
